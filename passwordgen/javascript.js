246af60684d133f4c48bb5f4304dd1e5fb2275e2
bronsongonzalez27 <122845938+bronsongonzalez27@users.noreply.github.com>
2023-03-21 17:25:13 -0700

    Add files via upload

---
+++ passwordgen/javascript.js
@@ -0,0 +1,48 @@
+// linked the html to javascript
+const characterAmount = document.getElementById
+('characterAmount')
+const numberAmount = document.getElementById
+('numberAmount')
+const includeUppercaseLettersElement = document.getElementById
+('includeUppercaseLetters')
+const includeNumbersElement = document.getElementById
+('includeNumbers')
+const includeSymbolsElement = document.getElementById
+('includeSymbol')
+const form=document.getElementById('passwordGeneratorButton')
+
+const SYMBOL_CHAR_CODES= arrayFromLowToHigh(33,47)
+const NUMBER_CHAR_CODES= arrayFromLowToHigh(48,57)
+const UPPERCASE_CHAR_CODES= arrayFromLowToHigh(97,122)
+const LOWERCASE_CHAR_CODES= arrayFromLowToHigh(65,90)
+
+
+// synced number up to the slider bar so both change
+characterAmount.addEventListener('input', syncCharacterAmount)
+numberAmount.addEventListener('input', syncCharacterAmount)
+
+form.addEventListener('submit', e =>{
+    e.preventDefault()
+    const characterAmount = numberAmount.value
+    const includeUppercaseLetters = includeUppercaseElement.checked
+    const includeSymbols = includeNumbersElement.checked
+    const includeNumbers = includeSymbolElement.checked
+        const password= generatePassword(characterAmount, includeUppercaseLetters,includeNumbers,includeSymbols)
+})
+// created function for generate password so password actually works
+function generatePassword(characterAmount,includeNumbers,includeUppercaseLetters,includeSymbol){
+     String.fromCharCode(65)
+}
+ function arrayFromHighToLow(low,high){
+    const array =[]
+    for (let i =low; i<=high; i++)
+    array.push(i)
+    return array
+ }
+
+
+function syncCharacterAmount(e){
+    const value = e.target.value
+    characterAmount.value= value
+    numberAmount.value =value
+}
